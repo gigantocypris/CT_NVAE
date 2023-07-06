3d5c1c14ce08f9e12b11e39b1a643a363a71f6f8
“gcgarychen25” <gcgarychen25@gmail.com>
2023-07-05 17:16:31 -0700

    updated shell scripts

--- scripts/loop_jobs.sh
+++ scripts/loop_jobs.sh
@@ -2,25 +2,3 @@
 for i in {2..3}; do
     sbatch $CT_NVAE_PATH/scripts/train_single_node.sh num_latent_scales_$i $i
 done
-
-
-'''#!/bin/bash
-
-# Define a new script file
-SCRIPT_FILE="run_all_jobs.sh"
-
-# Start the new script file with a shebang
-echo "#!/bin/bash" > $SCRIPT_FILE
-
-# Loop through your tasks
-for i in {2..4}; do
-    # Add each task to the new script file
-    echo "$CT_NVAE_PATH/scripts/train_single_node.sh num_latent_scales_$i $i" >> $SCRIPT_FILE
-done
-
-# Make the new script file executable
-chmod +x $SCRIPT_FILE
-
-# Submit the new script file as a single SLURM job
-sbatch $SCRIPT_FILE
-'''
